cdf6bd5a2d84c51d2aae307ec930380ea248cecd
Jenny <evgkatsada@gmail.com>
2022-02-25 17:01:33 +0200

    Authenticate user before any actions and hide table links for unauthenticated users

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,6 @@
 class ExpensesController < ApplicationController
   before_action :set_expense, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /expenses or /expenses.json
   def index
